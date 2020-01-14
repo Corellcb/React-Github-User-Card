948346b9575b8ea34e6bd9eb4538d81f64048c50
CorellCB <corellcb@gmail.com>
2020-01-14 17:14:27 -0500

    added usercard on mount

--- github-user-card/src/App.js
+++ github-user-card/src/App.js
@@ -1,18 +1,35 @@
 import React, { Component } from 'react';
+import axios from 'axios';
+import UserCard from './component/usercard';
+
 import './App.css';
 
 class App extends Component {
   constructor() {
     super();
     this.state = {
-      login: 'Corellcb'
+      gUser: []
     }
   }
+
+  componentDidMount() {
+    axios
+      .get('https://api.github.com/users/corellcb')
+      .then(response => {
+        this.setState({gUser: response.data});
+        console.log(this.state.gUser);
+      })
+      .catch(error => {
+        console.log(error);
+      })
+  }
+
   render() {
     return (
       <div className="App">
         <h1>Hello World!</h1>
-        <h2>Start editing to see some magic happen!</h2>
+        <UserCard gUser={this.state.gUser} />
+
       </div>
     );
   }
